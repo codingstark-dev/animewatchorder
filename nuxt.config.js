9dbd9f300642bde1d75f16661905d924ead3a3a8
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2020-12-20 04:08:56 +0530

    pwa bug fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,14 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    "@nuxtjs/tailwindcss"
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/pwa"
   ],
+  pwa: {
+    workbox: {
+      clientsClaim: false
+    }
+  },
   tailwindcss: {
     config: {
       theme: {},
